f9f87258fabd23b940f2bc597bd3e327ea78cd6d
Sergey <xxsmotur@gmail.com>
2019-03-18 20:55:26 +0500

    complete of implementing algorithm

--- jump/jump.py
+++ jump/jump.py
@@ -1,5 +1,4 @@
 
-import numpy as np
 
 def jump(key, buckets):
     """implementation of consistent hashing algorithm
@@ -7,15 +6,8 @@ def jump(key, buckets):
     """
     result = -1
     i = 0
-    key = np.int64(key)
     while i < buckets:
         result = i
-        key = key * 2862933555777941757 + 1
-        i = ((result + 1) * (1 << 31))/((divv(key, 33)) + 1)
-        print(i)
+        key = (key * int(2862933555777941757) + 1) & 0xffffffffffffffff
+        i = int(((result + 1) * (1 << 31))/((key >> 33) + 1))
     return result
-
-def divv(x, times):
-    for i in range(times):
-        x /= 2
-    return x 
