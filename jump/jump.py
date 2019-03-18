c6c1e54b828c67cdeec0ef300e1c50e1847380f6
Sergey <xxsmotur@gmail.com>
2019-03-18 20:43:26 +0500

    add numpy to requirements

--- jump/jump.py
+++ jump/jump.py
@@ -1,14 +1,18 @@
 
+import numpy as np
+
 def jump(key, buckets):
     """implementation of consistent hashing algorithm
     this algorithm described on this http://arxiv.org/abs/1406.2294
     """
     result = -1
     i = 0
+    key = np.int64(key)
     while i < buckets:
         result = i
-        key *= 2862933555777941757 + 1
-        i = (result + 1) * ((1 << 31)/(divv(key, 33)) + 1)
+        key = key * 2862933555777941757 + 1
+        i = ((result + 1) * (1 << 31))/((divv(key, 33)) + 1)
+        print(i)
     return result
 
 def divv(x, times):
