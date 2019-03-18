08f915c7ddbbf734c81db84b0dbcbcaf4f0f98d6
Sergey <xxsmotur@gmail.com>
2019-03-18 17:04:49 +0500

    implementation of the algorithm

---
+++ jump/jump.py
@@ -0,0 +1,12 @@
+
+def jump(key, buckets):
+    """implementation of consistent hashing algorithm
+    this algorithm described on this http://arxiv.org/abs/1406.2294
+    """
+    result = -1
+    i = 0
+    while i < buckets:
+        result = i
+        key *= 2862933555777941757 + 1
+        i = result + 1 * ((1 << 31)/(key >> 33) + 1)
+    return result 
